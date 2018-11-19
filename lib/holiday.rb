b65e8cee8495ee2c9beab84a7d8eab55c8a165ea
Claire Nguyen <ms.clairenguyen@gmail.com>
2018-11-19 03:54:39 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,6 +69,14 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
+holiday_hash.each do |season, holiday|
+  holiday.each do |supply|
+    
+  end 
+  
+end
+
+
 end
 
 def all_holidays_with_bbq(holiday_hash)
